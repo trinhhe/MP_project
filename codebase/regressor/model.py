4a76eeab4815659335000d17d3a55740c3fd4627
trinhhe <henry.trinh94@gmail.com>
2021-06-18 04:09:34 +0200

    LinearModel and dscriminiators from hmr implementation

--- codebase/regressor/model.py
+++ codebase/regressor/model.py
@@ -1,11 +1,16 @@
 from abc import ABC
 import torch
 from torch import nn
-from .body_model import BodyModel
+import torch.nn.functional as F
+from body_model import BodyModel
+from LinearModel import LinearModel
 from torch import hub
 import torchvision.models as models
 import numpy as np
 import h5py
+import util
+import sys
+
 
 class BaseModel(nn.Module, ABC):
     def __init__(self, cfg):
@@ -84,12 +89,14 @@ class BaseModel(nn.Module, ABC):
                                betas=betas)
 
         joints = body.Jtr
-        v_a_pose = body.v_a_pose
-        f = body.f
-        abs_bone_transforms = body.abs_bone_transforms
-        bone_transforms = body.bone_transforms
-        betas = body.betas
-        return joints, v_a_pose, f, abs_bone_transforms, bone_transforms, betas
+        # v_a_pose = body.v_a_pose
+        # f = body.f
+        # abs_bone_transforms = body.abs_bone_transforms
+        # bone_transforms = body.bone_transforms
+        # betas = body.betas
+        # return joints, v_a_pose, f, abs_bone_transforms, bone_transforms, betas
+        return joints
+
 
 class ParameterRegressor(nn.Module):
 
@@ -97,17 +104,18 @@ class ParameterRegressor(nn.Module):
         super().__init__()
         # self.batch_size = batch_size
         # In HMR implementation (https://github.com/MandyMo/pytorch_HMR) they initialize with mean_theta (all smpl parameters concatened)
-        # Dunno how to get them without downloading their file, so I just get some random numbers between -0.3;0.3 and increase iterations
         # init_theta = torch.from_numpy((np.random.random_sample(82) * (0.3+0.3) - 0.3).astype('float32'))
-        mean_params = h5py.File("../configs/neutral_smpl_mean_params.h5", mode="r")
-        init_theta = np.zeros(82, dtype = np.float)
+        mean_params = h5py.File(
+            "../configs/neutral_smpl_mean_params.h5", mode="r")
+        init_theta = np.zeros(82, dtype=np.float)
         init_theta[0:72] = mean_params['pose'][:]
         init_theta[72:] = mean_params['shape'][:]
-        self.register_buffer('init_theta', torch.from_numpy(init_theta).float())
+        self.register_buffer(
+            'init_theta', torch.from_numpy(init_theta).float())
 
         self.fc1 = nn.Linear(feature_count + 82, 1024)
         self.drop1 = nn.Dropout()
-        self.fc2 = nn.Linear(1024,1024)
+        self.fc2 = nn.Linear(1024, 1024)
         self.drop2 = nn.Dropout()
         self.dec_root_orient = nn.Linear(1024, 3)
         self.dec_pos_body = nn.Linear(1024, 63)
@@ -120,16 +128,17 @@ class ParameterRegressor(nn.Module):
         '''
         batch_size = input.shape[0]
         pred_theta = self.init_theta.expand(batch_size, -1)
-        pred_root_orient = pred_theta[:,:3]
-        pred_pos_body = pred_theta[:,3:66]
-        pred_pos_hand = pred_theta[:,66:72]
-        pred_beta = pred_theta[:,72:]        
-        
+        pred_root_orient = pred_theta[:, :3]
+        pred_pos_body = pred_theta[:, 3:66]
+        pred_pos_hand = pred_theta[:, 66:72]
+        pred_beta = pred_theta[:, 72:]
+
+        # in HMR they use relu activation functions but not in SPIN where they also make use of the HMR (not sure how this impacts the model)
         for i in range(iterations):
             input_c = torch.cat([input, pred_theta], 1)
-            input_c = self.fc1(input_c)
+            input_c = F.relu(self.fc1(input_c))
             input_c = self.drop1(input_c)
-            input_c = self.fc2(input_c)
+            input_c = F.relu(self.fc2(input_c))
             input_c = self.drop2(input_c)
             pred_root_orient = self.dec_root_orient(input_c) + pred_root_orient
             pred_pos_body = self.dec_pos_body(input_c) + pred_pos_body
@@ -138,12 +147,14 @@ class ParameterRegressor(nn.Module):
 
         return pred_root_orient, pred_pos_body, pred_pos_hand, pred_beta
 
+
 class Identity(nn.Module):
     def __init__(self):
         super(Identity, self).__init__()
-        
+
     def forward(self, x):
         return x
+
 
 class ConvModel_Pre(BaseModel):
     def __init__(self, cfg):
@@ -156,7 +167,8 @@ class ConvModel_Pre(BaseModel):
         """ Creates NNs. """
 
         print(f'Loading resnet_50 model...')
-        self.backbone = models.resnet50(pretrained=True)  #hub.load('pytorch/vision:v0.9.0', 'inception_v3', pretrained=True)
+        # hub.load('pytorch/vision:v0.9.0', 'inception_v3', pretrained=True)
+        self.backbone = models.resnet50(pretrained=True)
 
         # train only the classifier layer
         for param in self.backbone.parameters():
@@ -164,11 +176,11 @@ class ConvModel_Pre(BaseModel):
         # fc_in = self.backbone.fc.in_features
         # fc_out = self.backbone_f_len
         # self.backbone.fc = nn.Linear(in_features=fc_in, out_features=fc_out)
-        
-        #no classifier, Iterative regressor takes the output of resnet which is average pooled
+
+        # no classifier, Iterative regressor takes the output of resnet which is average pooled
         num_ftrs = self.backbone.fc.in_features
         self.backbone.fc = Identity()
-        
+
         self.regressor = ParameterRegressor(num_ftrs)
 
     def forward(self, input_data):
@@ -186,10 +198,12 @@ class ConvModel_Pre(BaseModel):
         # print(img_encoding.shape)
         # regress parameters
         iterations = 5
-        root_orient, pose_body, pose_hand, betas = self.regressor(img_encoding, iterations)
+        root_orient, pose_body, pose_hand, betas = self.regressor(
+            img_encoding, iterations)
 
         # regress vertices
-        vertices = self.get_vertices(root_loc, root_orient, betas, pose_body, pose_hand)
+        vertices = self.get_vertices(
+            root_loc, root_orient, betas, pose_body, pose_hand)
 
         predictions = {'vertices': vertices,
                        'root_loc': root_loc,
@@ -199,6 +213,128 @@ class ConvModel_Pre(BaseModel):
                        'pose_hand': pose_hand}
 
         return predictions
+
+
+'''
+https://github.com/MandyMo/pytorch_HMR/blob/master/src/Discriminator.py
+
+'''
+
+
+class ShapeDiscriminator(LinearModel):
+    def __init__(self, fc_layers, use_dropout, drop_prob, use_ac_func):
+        if fc_layers[-1] != 1:
+            msg = 'the neuron count of the last layer must be 1, but got {}'.format(fc_layers[-1])
+            sys.exit(msg)
+        super().__init__(fc_layers, use_dropout, drop_prob, use_ac_func)
+
+    def forward(self, inputs):
+        ''' 
+        Input: B x 10
+        Output: B x 1
+
+        '''
+        return self.fc_blocks(inputs)
+
+
+class PoseDiscriminator(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+
+        if channels[-1] != 1:
+            msg = 'the neuron count of the last layer must be 1, but got {}'.format(
+                channels[-1])
+            sys.exit(msg)
+
+        self.conv_blocks = nn.Sequential()
+        l = len(channels)
+        for idx in range(l - 2):
+            self.conv_blocks.add_module(
+                name='conv_{}'.format(idx),
+                module=nn.Conv2d(
+                    in_channels=channels[idx], out_channels=channels[idx + 1], kernel_size=1, stride=1)
+            )
+
+        self.fc_layer = nn.ModuleList()
+        for idx in range(23):
+            self.fc_layer.append(
+                nn.Linear(in_features=channels[l - 2], out_features=1))
+
+    def forward(self, inputs):
+        '''
+        Input: B x 23 x 9
+        Output: B x 1 and B x c x 1 x 23
+        '''
+        batch_size = inputs.shape[0]
+        inputs = inputs.transpose(1, 2).unsqueeze(2)  # to B x 9 x 1 x 23
+        internal_outputs = self.conv_blocks(inputs)  # to B x c x 1 x 23
+        o = []
+        for idx in range(23):
+            o.append(self.fc_layer[idx](internal_outputs[:, :, 0, idx]))
+
+        return torch.cat(o, 1), internal_outputs
+
+
+class FullPoseDiscriminator(LinearModel):
+    def __init__(self, fc_layers, use_dropout, drop_prob, use_ac_func):
+        if fc_layers[-1] != 1:
+            msg = 'the neuron count of the last layer must be 1, but got {}'.format(fc_layers[-1])
+            sys.exit(msg)
+
+        super(FullPoseDiscriminator, self).__init__(
+            fc_layers, use_dropout, drop_prob, use_ac_func)
+
+    def forward(self, inputs):
+        return self.fc_blocks(inputs)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self._create_sub_modules()
+
+    def _create_sub_modules(self):
+        '''
+            create theta discriminator for 23 joint
+        '''
+
+        self.pose_discriminator = PoseDiscriminator([9, 32, 32, 1])
+        
+        '''
+            create full pose discriminator for total 23 joints
+        '''
+        fc_layers = [23 * 32, 1024, 1024, 1]
+        use_dropout = [False, False, False]
+        drop_prob = [0.5, 0.5, 0.5]
+        use_ac_func = [True, True, False]
+        self.full_pose_discriminator = FullPoseDiscriminator(fc_layers, use_dropout, drop_prob, use_ac_func)
+
+        '''
+            shape discriminator for betas
+        '''
+        fc_layers = [10, 5, 1]
+        use_dropout = [False, False]
+        drop_prob = [0.5, 0.5]
+        use_ac_func = [True, False]
+        self.shape_discriminator = ShapeDiscriminator(fc_layers, use_dropout, drop_prob, use_ac_func)
+
+        print('finished create the discriminator modules...')
+
+
+    def forward(self, thetas):
+        '''
+        inputs is B x 82(72 + 10)
+        output: B x 25 (23 joints + 1 full pose + 1 betas)
+        '''
+        batch_size = thetas.shape[0]
+        # cams, poses, shapes = thetas[:, :3], thetas[:, 3:75], thetas[:, 75:]
+        poses, shapes = thetas[:, 0:72], thetas[:, 72:]
+        shape_disc_value = self.shape_discriminator(shapes)
+        rotate_matrixs = util.batch_rodrigues(poses.contiguous().view(-1, 3)).view(-1, 24, 9)[:, 1:, :] # We don't consider the root orient
+        pose_disc_value, pose_inter_disc_value = self.pose_discriminator(rotate_matrixs)
+        full_pose_disc_value = self.full_pose_discriminator(pose_inter_disc_value.contiguous().view(batch_size, -1))
+        return torch.cat((pose_disc_value, full_pose_disc_value, shape_disc_value), 1)
+
 
 class ConvModel(BaseModel):
     def __init__(self, cfg):
@@ -210,12 +346,16 @@ class ConvModel(BaseModel):
     def _build_net(self):
         """ Creates NNs. """
         print(f'Loading stock 2d-CNN model...')
-        fc_in_ch = 1*(self.img_resolution[0]//2**3)*(self.img_resolution[1]//2**3)
+        fc_in_ch = 1*(self.img_resolution[0] //
+                      2**3)*(self.img_resolution[1]//2**3)
 
         self.backbone = nn.Sequential(nn.Conv2d(self.in_ch, 5, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)), nn.ReLU(),
-                                      nn.Conv2d(5, 5, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)), nn.ReLU(),
-                                      nn.Conv2d(5, 5, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)), nn.ReLU(),
-                                      nn.Conv2d(5, 1, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)), nn.ReLU(),
+                                      nn.Conv2d(5, 5, kernel_size=(3, 3), stride=(
+                                          2, 2), padding=(1, 1)), nn.ReLU(),
+                                      nn.Conv2d(5, 5, kernel_size=(3, 3), stride=(
+                                          2, 2), padding=(1, 1)), nn.ReLU(),
+                                      nn.Conv2d(5, 1, kernel_size=(3, 3), stride=(
+                                          2, 2), padding=(1, 1)), nn.ReLU(),
                                       nn.Flatten(),
                                       nn.Linear(fc_in_ch, self.backbone_f_len))
 
@@ -243,7 +383,8 @@ class ConvModel(BaseModel):
         pose_hand = self.nn_pose_hand(img_encoding)
 
         # regress vertices
-        vertices = self.get_vertices(root_loc, root_orient, betas, pose_body, pose_hand)
+        vertices = self.get_vertices(
+            root_loc, root_orient, betas, pose_body, pose_hand)
 
         predictions = {'vertices': vertices,
                        'root_loc': root_loc,
@@ -254,10 +395,11 @@ class ConvModel(BaseModel):
 
         return predictions
 
+
 if __name__ == '__main__':
-    x = torch.rand(10,500).float()
-    net = ParameterRegressor(x.shape[1])
-    a,b,c,d = net(x,2)
+    # x = torch.rand(10, 500).float()
+    # net = ParameterRegressor(x.shape[1])
+    # a, b, c, d = net(x, 2)
     # print(a.shape)
     # print(net.named_buffers)
     # mean_params = np.load("../../configs/smpl_mean_params.npz")
@@ -267,6 +409,33 @@ if __name__ == '__main__':
     # print(mean_params['pose'].shape)
     # print(init_pose.shape)
     # print(mean_params.keys())
+   
+    # mean_values = h5py.File("../../configs/neutral_smpl_mean_params.h5")
+   
+    # mean = np.zeros(85, dtype = np.float)
+    # poses = mean_values['pose']
+    # mean[3:75] = poses[:]
+    # mean[75:] = mean_values['shape'][:]
+    # mean = torch.from_numpy(mean).float()
+    # mean = mean.expand(5, -1)
+    # print(mean.shape)
+    # dis = Discriminator()
+    # a = dis(mean)
+    # print(a.shape)
+    # poses, shapes = mean[:, 3:75], mean[:, 75:]
+    # print(poses.shape)
+    # print(poses.contiguous().view(-1,3).shape)
+    # rotate_matrixs = util.batch_rodrigues(poses.contiguous().view(-1, 3))
+    # print(rotate_matrixs.shape)
+    # print(rotate_matrixs.view(-1,24,9)[:, 1:, :].shape)
+    # print(poses.contiguous().view(-1, 3).shape)
+    # rotate_matrixs = util.batch_rodrigues(poses.contiguous().view(-1, 3))
+    # print(rotate_matrixs.shape)
+    # print(rotate_matrixs.view(-1, 24, 9)[:, 1:, :].shape)
 
-    mean_values = h5py.File("../configs/neutral_smpl_mean_params.h5")
-    print(mean_values['pose'].shape)
+    # pose_disc = PoseDiscriminator([9, 32, 32, 1])
+    # print(pose_disc)
+
+    # x = torch.rand(19, 3, 1, 23)
+    # a = x.contiguous().view(19, -1)
+    # print(a.shape)
--- codebase/regressor/util.py
+++ codebase/regressor/util.py
@@ -29,15 +29,47 @@ def proj_vertices(points, images, fx, fy, cx, cy):
 
     # discard invalid pixels
     valid = ~((x < 0) | (y < 0) | (x >= W) | (y >= H))
-    idx = (W*y + x) + W*H*torch.arange(0, B, device=device, dtype=torch.long).view(B, 1)
+    idx = (W*y + x) + W*H*torch.arange(0, B,
+                                       device=device, dtype=torch.long).view(B, 1)
     idx = idx.view(-1)[valid.view(-1)]
 
     # mark selected pixels
     rendered_img = torch.clone(images).permute(0, 2, 3, 1)  # -> (B, H, W, 3)
     rendered_img = rendered_img.contiguous().view(-1, 3)
-    rendered_img[idx] = torch.IntTensor([0, 0, 255]).to(device=device, dtype=rendered_img.dtype)
+    rendered_img[idx] = torch.IntTensor([0, 0, 255]).to(
+        device=device, dtype=rendered_img.dtype)
     rendered_img = rendered_img.view(B, H, W, 3).contiguous()
 
     rendered_img = rendered_img.permute(0, 3, 1, 2)
 
     return rendered_img
+
+
+def batch_rodrigues(aa_rots):
+    """
+    convert batch of rotations in axis-angle representation to matrix representation
+    :param aa_rots: Nx3
+    :return: mat_rots: Nx3x3
+    """
+
+    dtype = aa_rots.dtype
+    device = aa_rots.device
+
+    batch_size = aa_rots.shape[0]
+
+    angle = torch.norm(aa_rots + 1e-8, dim=1, keepdim=True)
+    rot_dir = aa_rots / angle
+
+    cos = torch.unsqueeze(torch.cos(angle), dim=1)
+    sin = torch.unsqueeze(torch.sin(angle), dim=1)
+
+    # Bx1 arrays
+    rx, ry, rz = torch.split(rot_dir, 1, dim=1)
+
+    zeros = torch.zeros((batch_size, 1), dtype=dtype, device=device)
+    K = torch.cat([zeros, -rz, ry, rz, zeros, -rx, -ry, rx, zeros], dim=1) \
+        .view((batch_size, 3, 3))
+
+    ident = torch.eye(3, dtype=dtype, device=device).unsqueeze(dim=0)
+    rot_mat = ident + sin * K + (1 - cos) * torch.bmm(K, K)
+    return rot_mat
